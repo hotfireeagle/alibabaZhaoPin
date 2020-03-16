06b0cc0e02101b2b485fc9893b0a20f7c49dedda
hotfireeagle <hotfireeagle@163.com>
2020-03-16 22:25:13 +0800

    update:close sign

--- src/components/sign/index.tsx
+++ src/components/sign/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
 import './index.scss'
+import { hide } from '../../services/loginService'
 
 export const SignComponent: React.FC<any> = () => {
+
+  const hideSign = () => hide()
+
   return (
-    <div className='mask'>
+    <div className='mask' onClick={hideSign}>
       
     </div>
   )
--- src/services/loginService.tsx
+++ src/services/loginService.tsx
@@ -4,7 +4,13 @@ import { SignComponent } from '../components/sign'
 
 export const show = () => {
   const div: HTMLDivElement = document.createElement('div')
+  div.id = 'signComponentWrapper'
   document.body.appendChild(div)
 
   ReactDOM.render(<SignComponent />, div)
+}
+
+export const hide = () => {
+  const div: HTMLElement | null = document.getElementById('signComponentWrapper')
+  div && document.body.removeChild(div)
 }
